565308b4dab4a602544644299239cba3705f1ccc
froggy _ <julia37_00@mail.ru>
2025-03-06 21:32:42 +0300

    fixed conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,17 @@
+import time
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+
 
 @pytest.fixture(scope="session")
 def browser():
    print("\nstart browser for test..")
-   options = Options()
-   options.add_argument('--ignore-certificate-errors')
-   browser = webdriver.Chrome(options=options)
-   #browser = webdriver.Chrome()
+   browser = webdriver.Chrome()
    yield browser
    alert_obj = browser.switch_to.alert
    msg = alert_obj.text
    print(msg)
    assert msg == 'Message received!'
+   time.sleep(10)
    print("\nquit browser..")
    browser.quit()
